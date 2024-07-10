e3c6ebbe4ea8110617788a60ae5fef93b6386c20
Valakyr159 <javiermoron159@hotmail.com>
2024-07-10 01:23:59 -0500

    Setup crud and dependencies

---
+++ src/main/java/com/valakyr/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,15 @@
+package com.valakyr.market.persistence.crud;
+
+import com.valakyr.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> getByCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int categoryId);
+}
